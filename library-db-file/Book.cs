0125148c914b17dd7179340430a48bc74df92f7d
Brunovini08 <brunovinicius082005@gmail.com>
2025-10-22 08:25:21 -0300

    Add project files.

---
+++ library-db-file/Book.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library_db_file
+{
+    public class Book
+    {
+        public int Id { get; private set; }
+        public string Title { get; private set; }
+        public string Author { get; private set; }
+        public string Category { get; private set; }
+
+        public Book(int id, string title, string author, string category)
+        {
+            this.Id = id;
+            this.Title = title;
+            this.Author = author;
+            this.Category = category;
+        }
+
+        public override string ToString()
+        {
+            return $"ID:{this.Id}\nAutor:{this.Author}\nTítulo:{this.Title}\nCategoria:{this.Category}\n";
+        }
+
+        public void setTitle(string title)
+        {
+            this.Title = title;
+        }
+
+        public void UpdteInformation(string title, string author, string category)
+        {
+            this.Title = title;
+            this.Author = author;
+            this.Category = category;
+        }
+
+        public string Save()
+        {
+            return $"{this.Id},{this.Author},{this.Title},{this.Category};";
+        }
+    }
+}
